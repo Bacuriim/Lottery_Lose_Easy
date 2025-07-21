d0700d38db2007da8b1589fbbb398b10778b9f5a
Einstein <conradoeinsteinpro@gmail.com>
2025-07-21 14:02:08 -0300

    ### Some critical alterations.

--- resources/backup.sql
+++ resources/backup.sql
@@ -8,10 +8,10 @@ CREATE TABLE Cliente (
    id UUID PRIMARY KEY,
    nome VARCHAR(100) NOT NULL,
    cpf VARCHAR(14) UNIQUE NOT NULL,
-   sexo CHAR(1),
-   idade INT,
-   endereco VARCHAR(255),
-   conta VARCHAR(30)
+   sexo CHAR(1) NOT NULL,
+   idade INT NOT NULL,
+   endereco VARCHAR(255) NOT NULL,
+   conta VARCHAR(30) NOT NULL
 );
 
 -- FUNCIONÁRIO
@@ -21,9 +21,9 @@ CREATE TABLE Funcionario (
     numero_identificacao VARCHAR(20) UNIQUE NOT NULL,
     salario FLOAT NOT NULL,
     cpf VARCHAR(14) UNIQUE NOT NULL,
-    sexo CHAR(1),
-    idade INT,
-    horario_trabalho VARCHAR(50)
+    sexo CHAR(1) NOT NULL,
+    idade INT NOT NULL,
+    horario_trabalho VARCHAR(50) NOT NULL
 );
 
 -- SERVIÇO
@@ -40,12 +40,12 @@ CREATE TABLE Servico (
 -- ATENDIMENTO
 CREATE TABLE Atendimento (
     id SERIAL PRIMARY KEY,
-    numero_transacao UUID,
-    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    numero_transacao UUID NOT NULL,
+    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     cliente_id UUID REFERENCES Cliente(id),
     funcionario_id UUID REFERENCES Funcionario(id),
     servico_id SERIAL REFERENCES Servico(id),
-    meio_pagamento VARCHAR(20),
+    meio_pagamento VARCHAR(20) NOT NULL,
     CHECK (meio_pagamento IS NULL OR meio_pagamento IN (
         'dinheiro',
         'boleto',
